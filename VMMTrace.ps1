cfcb8ce3cfc12d615d2b80fda396a2c5395d2386
Bill Curtis <wcurtis@wcurtis.net>
2020-11-12 10:27:57 -0500

    fixed some formatting

--- VMMTrace.ps1
+++ VMMTrace.ps1
@@ -7,43 +7,43 @@ $guid = (New-Guid).Guid
 
 $trace = Invoke-Command -ComputerName $ActiveVMMServer -ArgumentList $guid -Credential $Cred -ScriptBlock {
 
-$guid = $args[0]
+    $guid = $args[0]
 
-$filePath = "C:\VMMTrace-$guid"
+    $filePath = "C:\VMMTrace-$guid"
 
-New-Item -ItemType Directory -Path $filepath | Out-Null
+    New-Item -ItemType Directory -Path $filepath | Out-Null
 
-$expression = "logman create trace VMM-$guid -v mmddhhmm -o $filepath\VMMLog_$env:computername.ETL -cnf 01:00:00 -p Microsoft-VirtualMachineManager-Debug -nb 10 250 -bs 16 -max 512" 
+    $expression = "logman create trace VMM-$guid -v mmddhhmm -o $filepath\VMMLog_$env:computername.ETL -cnf 01:00:00 -p Microsoft-VirtualMachineManager-Debug -nb 10 250 -bs 16 -max 512" 
 
-Invoke-Expression $expression | Out-Null
+    Invoke-Expression $expression | Out-Null
 
-$expression = "logman start VMM-$guid"
+    $expression = "logman start VMM-$guid"
 
-Invoke-Expression $expression | Out-Null
+    Invoke-Expression $expression | Out-Null
 
-Read-Host -Prompt "Trace Started. Press ENTER to stop Trace"
+    Read-Host -Prompt "Trace Started. Press ENTER to stop Trace"
 
-$expression = "logman stop VMM-$guid"
+    $expression = "logman stop VMM-$guid"
 
-Invoke-Expression $expression | Out-Null
+    Invoke-Expression $expression | Out-Null
 
-$expression = "logman delete VMM-$guid"
+    $expression = "logman delete VMM-$guid"
 
-Invoke-Expression $expression | Out-Null
+    Invoke-Expression $expression | Out-Null
 
-$etlFile = Get-ChildItem $filePath -Filter *.etl
+    $etlFile = Get-ChildItem $filePath -Filter *.etl
 
-$expression = "netsh trace convert $($etlFile.FullName)"
+    $expression = "netsh trace convert $($etlFile.FullName)"
 
-Invoke-Expression $expression | Out-Null
+    Invoke-Expression $expression | Out-Null
 
-$txtFile = Get-ChildItem $filePath -Filter *.txt
+    $txtFile = Get-ChildItem $filePath -Filter *.txt
 
-$fulltrace = Get-Content -Path $txtFile.FullName
+    $fulltrace = Get-Content -Path $txtFile.FullName
 
-Remove-Item $filePath -Recurse -Force | Out-Null
+    Remove-Item $filePath -Recurse -Force | Out-Null
  
-return $fulltrace
+    return $fulltrace
 
 }
 
