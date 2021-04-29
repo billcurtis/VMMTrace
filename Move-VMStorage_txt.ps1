72aba8f85567a0bc43730a88c3451033aba840d4
Bill Curtis <{ID}+{username}@users.noreply.github.com>
2021-04-29 14:52:41 -0400

    initial commit

---
+++ Move-VMStorage_txt.ps1
@@ -0,0 +1,56 @@
+# Quickly written script. 4/29/2021
+
+# Just run script in a VMM PowerShell environment. 
+
+param (
+
+$TextFilePath
+
+)
+
+function move-vmmStorage {
+
+    param (
+    
+        $VMName,
+        $DestinationFileShare
+    
+    )
+    
+    # Load Modules
+    $ErrorActionPreference = "Stop"
+    $VerbosePreference = "SilentlyContinue"
+    Import-Module virtualmachinemanager
+    $VerbosePreference = "Continue"
+    
+    
+    $VM = Get-SCVirtualMachine -Name $VMName
+    $VMHost = Get-SCVMHost -ID $VM.HostId
+    
+    Move-SCVirtualMachine -VM $VM -UseLAN -RunAsynchronously -VMHost $VMHost -Path $DestinationFileShare 
+    
+}
+    
+
+# Load Modules
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "SilentlyContinue"
+Import-Module virtualmachinemanager
+$VerbosePreference = "Continue"
+
+# Import Text File
+$vmDataSet = Import-Csv -Delimiter " " -Path $TextFilePath -Header Computer    
+
+$title = "Please select the File Share to migrate to:"
+$DestinationFileShare = (Get-SCStorageFileShare | Select-Object SharePath) | Out-GridView -Title $title -OutputMode Single
+    
+Write-Output $vmDataSet
+Write-Verbose "Destination File Share is: $($DestinationFileShare.SharePath)"
+
+foreach ($vmData in $vmDataSet.Computer) {
+    
+    Write-Verbose "Moving virtual machine: $($vmData)"
+    move-vmmStorage -VMName $vmData.Name -DestinationFileShare $DestinationFileShare.SharePath
+    
+}
+     
